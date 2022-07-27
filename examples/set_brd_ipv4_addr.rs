6502d25666cab5b1dfd53b62391ddd17f56f262e
M. Amin Rayej <m.amin.rayej@gmail.com>
2022-07-27 18:16:18 +0430

    clean up the examples

--- examples/set_brd_ipv4_addr.rs
+++ examples/set_brd_ipv4_addr.rs
@@ -1,17 +1,15 @@
+use std::net::Ipv4Addr;
+
 use tidy_tuntap::iface;
 
 fn main() {
     let iface = iface::Interface::new("tun10", iface::Mode::Tun, true).unwrap();
 
-    iface
-        .set_addr(std::net::Ipv4Addr::new(128, 0, 0, 1))
-        .unwrap();
-    iface
-        .set_brd_addr(std::net::Ipv4Addr::new(129, 0, 0, 1))
-        .unwrap();
+    iface.set_addr(Ipv4Addr::new(10, 10, 10, 10)).unwrap();
+    iface.set_brd_addr(Ipv4Addr::new(10, 10, 255, 255)).unwrap();
 
     assert_eq!(
         iface.get_brd_addr().unwrap(),
-        std::net::Ipv4Addr::new(129, 0, 0, 1)
+        Ipv4Addr::new(10, 10, 255, 255)
     );
 }
--- examples/set_netmask.rs
+++ examples/set_netmask.rs
@@ -1,17 +1,12 @@
+use std::net::Ipv4Addr;
+
 use tidy_tuntap::iface;
 
 fn main() {
     let iface = iface::Interface::new("tun10", iface::Mode::Tun, true).unwrap();
 
-    iface
-        .set_addr(std::net::Ipv4Addr::new(128, 0, 0, 1))
-        .unwrap();
-    iface
-        .set_netmask(std::net::Ipv4Addr::new(255, 255, 0, 0))
-        .unwrap();
+    iface.set_addr(Ipv4Addr::new(10, 10, 10, 10)).unwrap();
+    iface.set_netmask(Ipv4Addr::new(255, 255, 0, 0)).unwrap();
 
-    assert_eq!(
-        iface.get_netmask().unwrap(),
-        std::net::Ipv4Addr::new(255, 255, 0, 0)
-    );
+    assert_eq!(iface.get_netmask().unwrap(), Ipv4Addr::new(255, 255, 0, 0));
 }
