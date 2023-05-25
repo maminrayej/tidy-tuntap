1b788f8f291ac107c046c7e8543a3df8d9b67cc2
Frostie314159 <iam.an.programmer@gmail.com>
2023-05-25 09:56:09 +0200

    Fixed incorrect variable name in example.

--- examples/set_hwaddr.rs
+++ examples/set_hwaddr.rs
@@ -1,11 +1,11 @@
 use tidy_tuntap::*;
 
 fn main() {
-    let tun = Tap::new("tun10", false).unwrap();
+    let tap = Tap::new("tun10", false).unwrap();
 
     let hwaddr = [0x00, 0x80, 0x41, 0x13, 0x37, 0x42];
 
-    tun.set_hwaddr(hwaddr).unwrap();
+    tap.set_hwaddr(hwaddr).unwrap();
 
-    assert_eq!(tun.get_hwaddr().unwrap(), hwaddr);
+    assert_eq!(tap.get_hwaddr().unwrap(), hwaddr);
 }
