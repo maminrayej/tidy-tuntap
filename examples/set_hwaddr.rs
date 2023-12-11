70ce4d897f7788874d34b5a28fe9a205c9be59bf
Frostie314159 <iam.an.programmer@gmail.com>
2023-12-11 16:49:43 +0100

    Fixed example.

--- examples/set_hwaddr.rs
+++ examples/set_hwaddr.rs
@@ -1,7 +1,7 @@
 use tidy_tuntap::*;
 
 fn main() {
-    let tap = Tap::new("tun10", false).unwrap();
+    let tap = Tap::new("tap10", false).unwrap();
 
     let hwaddr = [0x00, 0x80, 0x41, 0x13, 0x37, 0x42];
 
